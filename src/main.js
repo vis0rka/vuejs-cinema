ef8a87abe268f6cc1acd9288e6af301966970b5d
Viktor <barta.viktor.ufs@gmail.com>
2019-05-11 11:10:37 +0200

    feat(filter) add movie filter

--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
 import Vue from 'vue';
 import './style.scss';
+import genres from './util/genres';
+import MovieList from './components/MovieList.vue';
+import MovieFilter from './components/MovieFilter.vue';
 
 new Vue({
   el: '#app',
   data: {
-    msg: 'hello world'
+    genre: [],
+    time: [],
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      if (checked) {
+        this[category].push(title);
+      } else {
+        let index = this[category].indexOf(title);
+        if (index > -1) {
+          this[category].splice(index, 1);
+        }
+      }
+    }
+  },
+  components: {
+    MovieList,
+    MovieFilter,
   }
 });
