457d03c8e7b2146f6ff81b8517dd9f60f9292e39
Viktor <barta.viktor.ufs@gmail.com>
2019-05-11 18:34:39 +0200

    feat(event bus) add checkfilter method to bus

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -3,11 +3,11 @@
     <h2>Filter results</h2>
     <h3>By time of day</h3>
     <div class="filter-group">
-      <check-filter v-for="time in times" category="time" v-bind:title="time" v-on:check-filter="checkFilter"></check-filter>
+      <check-filter v-for="time in times" category="time" v-bind:title="time"></check-filter>
     </div>
     <h3>By genre</h3>
         <div class="filter-group">
-      <check-filter v-for="genre in genres" category="genre" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+      <check-filter v-for="genre in genres" category="genre" v-bind:title="genre"></check-filter>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import './style.scss';
 import MovieList from './components/MovieList.vue';
 import MovieFilter from './components/MovieFilter.vue';
 
+import { checkFilter } from './util/bus';
+
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
 import moment from 'moment-timezone';
 moment.tz.setDefault("UTC");
 Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
+
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', { get() { return this.$root.bus } });
 
 new Vue({
   el: '#app',
@@ -18,19 +23,9 @@ new Vue({
     movies: [],
     moment,
     day: moment(),
+    bus
   },
-  methods: {
-    checkFilter(category, title, checked) {
-      if (checked) {
-        this[category].push(title);
-      } else {
-        let index = this[category].indexOf(title);
-        if (index > -1) {
-          this[category].splice(index, 1);
-        }
-      }
-    }
-  },
+  methods: {  },
   components: {
     MovieList,
     MovieFilter,
@@ -39,6 +34,8 @@ new Vue({
     fetch('/api', { method: 'GET' })
       .then(dataWrappedByPromise => dataWrappedByPromise.json())
       .then(resdata => this.movies = resdata);
+
+    this.$bus.$on('check-filter', checkFilter.bind(this));
   }
 });
 
